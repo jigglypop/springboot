bb20929b2ed0ff70f3002d6233a19c287aab8c94
jigglypop <ydh2244@gmail.com>
2020-05-05 21:25:38 +0900

    0505

---
+++ server/src/main/java/com/ssafyblog/SsafyblogApplication.java
@@ -0,0 +1,13 @@
+package com.ssafyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyblogApplication.class, args);
+	}
+
+}
